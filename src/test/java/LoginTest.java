87d9115e6575b2651081e695ce24c7339e46cf3c
Alena1-1-1 <dobrenkayaa2000@gmail.com>
2025-02-07 16:31:37 +0300

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -44,5 +44,25 @@ public class LoginTest {
         Assert.assertTrue(title);
         driver.quit();
     }
+/*  Ввести неверный логин, пароль оставить корректным
+    1.Открываем браузер
+    2.Вводим в строку поиска  https://www.saucedemo.com/
+    3.Ввожим в поле user - user_standard
+    4.Ввожим в поле - secret_sauce
+    5.Жмем кнопку login
+    6.Проверяем, что мы получили ошибку - Epic sadface: Username and password do not match any user in this service
+     */
 
+    @Test
+    public void pypypyTest() {
+        WebDriver driver = new ChromeDriver(); /*заходит в Хром*/
+        driver.manage().window().maximize();   /*откроет страницу полноэкранный режим*/
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));  /*ждет 10 сек, чтобы продолжить*/
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("user_standard"); /*находит элемент по айдишке и вводит*/
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String error = driver.findElement(By.cssSelector("[data-test=error]")).getText(); /*тк нет айдишки в коде, то так*/
+        Assert.assertEquals(error, "Epic sadface: Username and password do not match any user in this service");
+        driver.quit();
 }
